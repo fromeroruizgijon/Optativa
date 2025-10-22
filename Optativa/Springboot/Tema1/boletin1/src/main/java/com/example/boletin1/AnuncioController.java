da2914056a749b02dc17a91c1c916e23781d7101
fromeroruizgijon <francisco.romero-velazquez@gmail.com>
2025-10-22 23:21:12 +0200

    no sé hacer que pasen los datos xd

--- Optativa/Springboot/Tema1/boletin1/src/main/java/com/example/boletin1/AnuncioController.java
+++ Optativa/Springboot/Tema1/boletin1/src/main/java/com/example/boletin1/AnuncioController.java
@@ -1,11 +1,13 @@
+package com.example.boletin1;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AnuncioController{
+public class AnuncioController {
 
     @GetMapping("/anuncio.html")
-    public String metodo(){
+    public String metodo() {
         return "anuncio";
-
     }
 }
