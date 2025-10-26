f5cb325594e58b11a99660112b97eae057881f68
fromeroruizgijon <francisco.romero-velazquez@iesruizgijon.com>
2025-10-26 15:03:19 +0100

    Ejercicios: 1, 2 y 3

--- Optativa/Springboot/Tema1/boletin1/src/main/java/com/example/boletin1/AnuncioController.java
+++ Optativa/Springboot/Tema1/boletin1/src/main/java/com/example/boletin1/AnuncioController.java
@@ -2,12 +2,17 @@ package com.example.boletin1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 @Controller
 public class AnuncioController {
 
-    @GetMapping("/anuncio.html")
-    public String metodo() {
+    @GetMapping("/anuncio")
+    public String metodo(@RequestParam(name = "nombre") String nombre, @RequestParam(name = "asunto")String asunto, @RequestParam(name = "cuerpo") String cuerpo, Model model) {
+        model.addAttribute("nombre", nombre);
+        model.addAttribute("asunto", asunto);
+        model.addAttribute("cuerpo", cuerpo);
         return "anuncio";
     }
 }
