25fc4386a48392f80f5eb0ecba53c580a4841c33
Francisco Romero <francisco.romero-velazquez@iesruizgijon.com>
2025-11-06 14:25:38 +0100

    css mal xd

---
+++ Optativa/Springboot/proyecto/demo/src/main/java/com/example/demo/TareaFix.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import java.sql.Date;
+
+public class TareaFix extends Tarea {
+    
+    public TareaFix(String titulo, String description, boolean estado, int prioridad, Date fechaCreacion) {
+        super(titulo, description, estado, prioridad, fechaCreacion);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
