28a17e84f71c7aa117c8e894e1400901555291bc
fromeroruizgijon <francisco.romero-velazquez@iesruizgijon.com>
2026-02-21 20:54:24 +0100

    permisos de swagger

--- Optativa/Springboot/proyecto/demo/src/main/java/com/example/demo/WebSecurityConfig.java
+++ Optativa/Springboot/proyecto/demo/src/main/java/com/example/demo/WebSecurityConfig.java
@@ -18,6 +18,7 @@ public class WebSecurityConfig {
         http
             .authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/", "/login", "/registro", "/insertarUsuario", "/css/**", "/js/**", "/images/**").permitAll()
+                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                 .requestMatchers("/admin/**").hasAuthority("admin")
                 .anyRequest().authenticated()
             )
